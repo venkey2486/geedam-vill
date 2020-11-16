71af88e58d9c9e34b3b927940548e7da214daceb
venkey2486 <venkey2486@gmail.com>
2020-11-16 10:05:47 +0530

    commit v6

--- backend/config.js
+++ backend/config.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/geedam',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://venkey2486:mydear123@@myvill.bn7sc.mongodb.net/geedam?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
 };
--- backend/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports={
-    mongoURI:'mongodb+srv://venkey2486:mydear123@@myvill.bn7sc.mongodb.net/<dbname>?retryWrites=true&w=majority'
-}
